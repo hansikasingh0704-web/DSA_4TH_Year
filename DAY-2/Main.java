bd790a450b625d1892a58404efeb442901d59960
hansika-singh <hansikasingh1804@gmail.com>
2026-07-06 14:29:07 +0530

    day-2

---
+++ DAY-2/Main.java
@@ -0,0 +1,83 @@
+
+
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+class LinkedList {
+
+    Node head;
+
+  
+    public void insertAtBeginning(int data){
+
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+
+        head = newNode;
+    }
+
+    
+    public void insertAtEnd(int data){
+
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+
+    public void display(){
+
+        Node temp = head;
+
+        while(temp != null){
+
+            System.out.print(temp.data + " -> ");
+
+            temp = temp.next;
+        }
+
+        System.out.println("NULL");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+
+        list.insertAtBeginning(5);
+
+        list.display();
+    }
+}
+
+
+
